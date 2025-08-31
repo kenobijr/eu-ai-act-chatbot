4ddacc0cb5103f03b8cd57b3d6f542df81fe7709
kenobijr <kenobijr@tatooine.com>
2025-08-31 14:58:17 +0200

    ragconfig comments

--- src/config.py
+++ src/config.py
@@ -30,30 +30,22 @@ _MESSAGES = _load_system_messages(file_path="data/system_messages.yml")
 
 @dataclass
 class RAGConfig:
-    """
-    - class to steer all relevant RAGPipeline parameters central
-    - provides also systemmessages for cases non-rag & rag-enriched
-    - includes token management parameters for TokenManager class
-    """
+    """ token management, sytemmessages, direct & semantic search parameters """
     # langchain model
     llm: str = "llama3-8b-8192"
+    # system prompts
+    system_message_rag_disabled: str = _MESSAGES["system_message_rag_disabled"]
+    system_message_rag_enabled: str = _MESSAGES["system_message_rag_enabled"]
     # context window and token management
     total_available_tokens: int = 6000  # groq TPM limit = 6k
     token_buffer: float = 0.12  # reduce token budget by ratio: tiktoken / llama 3 tokenizer
     system_prompt_tokens: int = 550
     formatting_overhead_tokens: int = 50
-    # allocation ratios
+    # token allocation ratios derived from total available token amount
     user_query_share: float = 0.2
     llm_response_share: float = 0.2
     rag_content_share: float = 0.6
-    # rag engine
-    nearest_articles: int = 30  # total number 113
-    nearest_annexes: int = 5  # total number 13
-    nearest_recitals: int = 15  # total number 180
-    nearest_definitions: int = 15  # total number 68
-    rel_threshold: float = 0.8  # rag relevance threshold for cosine distance
-    relationship_boost: float = 0.5  # relationship boost factor (multiplication)
-    # direct search
+    # direct search -> max matches within one user prompt; search patterns / entities
     max_direct_matches: int = 3  # maximum number of direct entity matches to extract from prompt
     search_configs: dict = field(default_factory=lambda: {
         "article": {
@@ -66,12 +58,17 @@ class RAGConfig:
             "collection": "annexes", "max_num": 13, "pad_digits": 2, "id_prefix": "annex_"
         },
     })
-    # system prompts
-    system_message_rag_disabled: str = _MESSAGES["system_message_rag_disabled"]
-    system_message_rag_enabled: str = _MESSAGES["system_message_rag_enabled"]
+    # semantic search -> get x nearest matches from db per entity type; filter down by relevance
+    nearest_articles: int = 30  # total number 113
+    nearest_annexes: int = 5  # total number 13
+    nearest_recitals: int = 15  # total number 180
+    nearest_definitions: int = 15  # total number 68
+    rel_threshold: float = 0.8  # rag relevance threshold for cosine distance
+    # relationship boost -> multiplicate cosine distance of boosted entities by factor
+    relationship_boost: float = 0.5
 
     def __post_init__(self):
-        # make sure token share percentages add up to 100%
+        """ make sure token share percentages add up to 100% """
         assert (
             self.user_query_share
             + self.llm_response_share
